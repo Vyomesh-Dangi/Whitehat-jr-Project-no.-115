2f0d9b95f36c975d8c8399cd42977fd89086adb8
Vyomesh Dangi <103869955+Vyomesh-Dangi@users.noreply.github.com>
2022-06-22 16:01:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPose);
+}
+
+function draw(){
+    image(video,0,0,300,300);
+}
+
+function take_snapshot(){
+    save("my_picture.png");
+}
+
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
+}
+
+function gotPose(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("nose x: "+results[0].pose.nose.x);
+        console.log("nose y: "+results[0].pose.nose.y);
+    };
+}
